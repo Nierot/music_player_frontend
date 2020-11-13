7dabf835ec7c1cff4245084425b8ba0e116a0fbd
Nierot <56843328+Nierot@users.noreply.github.com>
2020-11-13 16:41:52 +0100

    frontend to start playing a playlist

--- src/playlist/PlayPlaylist.js
+++ src/playlist/PlayPlaylist.js
@@ -6,9 +6,16 @@ export default class PlayPlaylist extends React.Component {
   
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      wantEvents: false,
+      songsBetweenEvents: 20,
+      listOfPeople: []
+    };
 
     this.toPlayer = this.toPlayer.bind(this);
+    this.handleEventChange = this.handleEventChange.bind(this);
+    this.handleNumberOfSongsChange = this.handleNumberOfSongsChange.bind(this);
+    this.parseList = this.parseList.bind(this);
   }
 
   toPlayer() {
@@ -16,23 +23,60 @@ export default class PlayPlaylist extends React.Component {
       alert('You are not logged into spotify! Sending you back to home to login');
       return window.location.replace('/');
     }
-    window.location.replace(`/player?p=${getQueryParam('p')}`);
+    if (this.state.listOfPeople.length === 0) {
+      return this.setState({ listOfPeople: ['List', 'Of', 'People', 'Undefined', 'Good', 'Job']})
+    }
+    window.location.replace(`/player?p=${getQueryParam('p')}&events=${this.state.wantEvents}&listOfPeople=${this.state.listOfPeople}&songsBetweenEvents=${this.state.songsBetweenEvents}`);
+  }
+
+  handleEventChange(e) {
+    this.setState({ wantEvents: e.target.checked });
+  }
+  
+  handleNumberOfSongsChange(e) {
+    this.setState({ songsBetweenEvents: e.target.value });
+  }
+
+  parseList(e) {
+    let text = e.target.value;
+    let people = text.split(',');
+    this.setState({ listOfPeople: people });
+  }
+
+  TableOfPeople() {
+    if (!this.state.listOfPeople) return null;
+    return (
+      <div className="TableOfPeople">
+        <table>
+          <tbody>
+            {this.state.listOfPeople.map(person => <tr key={Math.random()}>{person}</tr>)}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 
   render() {
 
     return (
       <div className="PlayPlaylist">
-        <h1>PlayPlaylist</h1>
-
-        <select>
-          <option>Shuffle</option>
-        </select>
-
-        <br />
+        <h1>Play options</h1>
 
         Events?:
-        <input type="checkbox" />
+        <input type="checkbox" id="wantEvents" onChange={this.handleEventChange}/>
+        
+        {this.state.wantEvents ?
+          <div className="wantEvents">
+            Number of songs between events:
+            <input type="number" defaultValue={20} onChange={this.handleNumberOfSongsChange}/>
+            <br/>
+            List of people participating in the events (comma-seperated):
+            <br />
+            <textarea id="listOfPeople" onChange={this.parseList}/>
+            {this.TableOfPeople()}
+          </div>
+        : null }
+
         <button className="button" onClick={this.toPlayer}>Play!</button>
       </div>
     )
